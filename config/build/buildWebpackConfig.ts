de3eaef3f39d8205164999b964bf3301d8c0a01f
denismiron <denisbvb0@gmail.com>
2024-01-29 23:27:06 +0300

    add webpack dev server

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -1,16 +1,16 @@
-import webpack from 'webpack';
-import path from 'path';
-import { BuildOptions } from './types/config';
-import { buildPlugins } from './buildPlugins';
-import { buildLoaders } from './buildLoader';
-import { buildResolvers } from './buildResolvers';
+import {BuildOptions} from "./types/config";
+import webpack from "webpack";
+import path from "path";
+import {buildPlugins} from "./buildPlugins";
+import {buildLoaders} from "./buildLoaders";
+import {buildResolvers} from "./buildResolvers";
+import {buildDevServer} from "./buildDevServer";
 
-export function buildWebpackConfig(options: BuildOptions): webpack.Configuration{
-    
-    const {paths, mode} = options;
+export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
+    const {paths, mode, isDev} = options;
 
     return {
-        mode,
+        mode: mode,
         entry: paths.entry,
         output: {
             filename: "[name].[contenthash].js",
@@ -22,5 +22,7 @@ export function buildWebpackConfig(options: BuildOptions): webpack.Configuration
             rules: buildLoaders(),
         },
         resolve: buildResolvers(),
+        devtool: isDev ? 'inline-source-map' : undefined,
+        devServer: isDev ? buildDevServer(options) : undefined,
     }
 }
