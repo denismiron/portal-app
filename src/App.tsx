ff5c68914c12d0ace39419e61b9321aaf6d35bc3
denismiron <denisbvb0@gmail.com>
2024-02-07 12:01:33 +0300

    add dark&light theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import './index.scss'
+import React, { useContext, useState } from 'react';
+import './styles/index.scss'
 import { Route, Routes, Link } from 'react-router-dom';
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
 import { Suspense } from 'react';
+import { useTheme } from './theme/useTheme';
 
 const App = () => {
+    const {theme, toggleTheme} = useTheme();
     return (
-        <div className='app'>
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>Theme</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback = {<div>Loading...</div>}>
