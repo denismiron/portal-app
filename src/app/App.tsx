fe765a2d6e7f15701f9bf0e7968e84d09708425e
denismiron <denisbvb0@gmail.com>
2024-02-16 16:23:20 +0300

    refactor router

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,6 +5,7 @@ import { classNames } from 'shared/lib/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { AboutPage } from 'pages/AboutPage';
 import { MainPage } from 'pages/MainPage';
+import { AppRouter } from './providers/router';
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
@@ -13,12 +14,7 @@ const App = () => {
             <button onClick={toggleTheme}>Theme</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
-            <Suspense fallback = {<div>Loading...</div>}>
-                <Routes>
-                    <Route path={'/about'} element={<AboutPage/>}/>
-                    <Route path={'/'} element={<MainPage/>}/>
-                </Routes>
-            </Suspense>
+            <AppRouter/>
         </div>
     );
 };
