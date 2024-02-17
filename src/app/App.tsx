9e13cb529d54ea86a53ecf9b031efe03ee3df1ae
denismiron <denisbvb0@gmail.com>
2024-02-17 18:04:42 +0300

    navbar widget & applink added

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,15 +6,15 @@ import { useTheme } from 'app/providers/ThemeProvider';
 import { AboutPage } from 'pages/AboutPage';
 import { MainPage } from 'pages/MainPage';
 import { AppRouter } from './providers/router';
+import { Navbar } from 'widgets/Navbar';
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
     return (
         <div className={classNames('app', {}, [theme])}>
-            <button onClick={toggleTheme}>Theme</button>
-            <Link to={'/'}>Главная</Link>
-            <Link to={'/about'}>О сайте</Link>
+            <Navbar/>
             <AppRouter/>
+            <button onClick={toggleTheme}>Theme</button>
         </div>
     );
 };
