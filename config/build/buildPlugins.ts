81acc41d1df8e28723896cb1ac7e878d98f4cfec
denismiron <denisbvb0@gmail.com>
2024-02-20 17:23:18 +0300

    add i18n

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,10 +1,10 @@
-import path from 'path';
-import webpack, { optimize } from 'webpack';
+
+import webpack from 'webpack';
 import HTMLWebpackPlugin from "html-webpack-plugin";
-import { BuildMode, BuildOptions } from './types/config';
+import { BuildOptions } from './types/config';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 
-export function buildPlugins({paths}: BuildOptions): webpack.WebpackPluginInstance[]{
+export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[]{
 
     return [
         new HTMLWebpackPlugin({
@@ -14,6 +14,9 @@ export function buildPlugins({paths}: BuildOptions): webpack.WebpackPluginInstan
         new MiniCssExtractPlugin({
             filename: 'css/[name].[contenthash:8].css',
             chunkFilename: 'css/[name].[contenthash:8].css',
+        }),
+        new webpack.DefinePlugin({
+            __IS_DEV__: JSON.stringify(isDev),
         })
     ]
 }
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,17 +8,35 @@ import { MainPage } from 'pages/MainPage';
 import { AppRouter } from './providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
+import { useTranslation } from 'react-i18next';
 
+const Component = () => {
+    const {t, i18n} = useTranslation();
+
+    const toggle = () =>{
+        i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru')
+    }
+
+    return (
+        <div>
+            <button onClick={toggle}>{t('Перевод')}</button>
+            {t('Тестовый перевод')}
+        </div>
+    )
+}
 
 const App = () => {
     const {theme} = useTheme();
     return (
         <div className={classNames('app', {}, [theme])}>
-            <Navbar/>
-            <div className='content-page'>
-                <Sidebar/>
-                <AppRouter/>
-            </div>
+            <Suspense fallback="">
+                <Navbar/>
+                <Component/>
+                    <div className='content-page'>
+                        <Sidebar/>
+                        <AppRouter/>
+                    </div>
+            </Suspense>
         </div>
     );
 };
